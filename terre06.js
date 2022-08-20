0befd5c268cce5a1015f1be0f9d3e3a1f0885f86
Pierre <pierrebossetti_348@hotmail.fr>
2022-08-20 13:29:34 +0200

    Exercice 06 correction erreur arguments

--- terre06.js
+++ terre06.js
@@ -4,4 +4,9 @@ let reverseString = '';
 for (let i = string.length - 1; i >= 0; i--) {
   reverseString += string[i];
 }
-console.log(reverseString);
+
+if (process.argv.length > 3) {
+  console.log('erreur. écris uniquement un argument stp.');
+} else {
+  console.log(reverseString);
+}
