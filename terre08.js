910fc8c0106ec9026cef422ef7fcfc179a0e340b
Pierre <pierrebossetti_348@hotmail.fr>
2022-08-23 21:56:24 +0200

    Exercice 08 correction nom des variables

--- terre08.js
+++ terre08.js
@@ -1,22 +1,22 @@
 const inputNumber = process.argv[2];
-const power = process.argv[3];
-let numberPowered = 0;
+const exposant = process.argv[3];
+let tempNumber = 0;
 
-const power = function (inputNumber, power) {
+const square = function (inputNumber, exposant) {
   const x = inputNumber;
   let i = 0;
-  while (i < power - 1) {
-    numberPowered = inputNumber * x;
-    inputNumber = numberPowered;
+  while (i < exposant - 1) {
+    tempNumber = inputNumber * x;
+    inputNumber = tempNumber;
     i++;
   }
-  return numberPowered;
+  return tempNumber;
 };
 
-if (power < 0) {
+if (exposant < 0) {
   console.log("L'exposant ne peut être négatif.");
-} else if (inputNumber == 0 && power == 0) {
+} else if (inputNumber == 0 && exposant == 0) {
   console.log(1);
 } else {
-  console.log(power(inputNumber, power));
+  console.log(square(inputNumber, exposant));
 }
