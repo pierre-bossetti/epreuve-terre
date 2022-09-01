2556aada009b96c0cb39f56102299613c2502ec0
Pierre <pierrebossetti_348@hotmail.fr>
2022-09-01 21:38:05 +0200

    Correction trié ou pas

--- terre014.js
+++ terre014.js
@@ -2,7 +2,7 @@ const number1 = process.argv[2] / 1;
 const number2 = process.argv[3] / 1;
 const number3 = process.argv[4] / 1;
 let numbers = [number1, number2, number3];
-console.log(number1, number2, number3);
+
 const sorted = function (list) {
   if (numbers[0] < numbers[1] && numbers[1] < numbers[2]) {
     return true;
@@ -11,9 +11,14 @@ const sorted = function (list) {
   }
 };
 
+let i = 0;
+while (process.argv[i]) {
+  i++;
+}
+
 const listVerification = sorted(numbers);
 
-if (process.argv.length != 5) {
+if (i != 5) {
   console.log('erreur.');
 } else if (listVerification == false) {
   console.log('Pas triée !');
