da01130bca1b9342df72ca1830939363e8ebbf72
Pierre <pierrebossetti_348@hotmail.fr>
2022-09-01 19:45:19 +0200

    Exercice 13 trouver la suisse correction

--- terre013.js
+++ terre013.js
@@ -1,12 +1,13 @@
-const number1 = process.argv[2];
-const number2 = process.argv[3];
-const number3 = process.argv[4];
+const number1 = process.argv[2] / 1;
+const number2 = process.argv[3] / 1;
+const number3 = process.argv[4] / 1;
 let numbers = [];
 const elements = 3;
 
+console.log(number1, typeof number1);
 //Ajoute le premier chiffre
 for (let i = 0; i < 1; i++) {
-  if (number1 < number2 && number1 < number2) {
+  if (number1 < number2 && number1 < number3) {
     numbers[i] = number1;
   } else if (number2 < number1 && number2 < number3) {
     numbers[i] = number2;
@@ -50,3 +51,5 @@ if (number1 === number2 && number2 === number3 && number1 === number3) {
 } else {
   console.log(numbers[(elements + 1) / 2 - 1]);
 }
+
+console.log(numbers);
